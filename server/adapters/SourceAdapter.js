f2ff73a436075c22f9564452c71f0f638fbab270
Lucas Eraldo <lucaseraldopm@gmail.com>
2026-03-01 16:58:29 -0300

    Fix Prisma v7 config

--- server/adapters/SourceAdapter.js
+++ server/adapters/SourceAdapter.js
@@ -4,7 +4,6 @@ const { PrismaPg } = require("@prisma/adapter-pg");
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
-// Mantém esses exports porque seu index.js importa eles
 const MockOfficialAdapter = { name: "MockOfficialAdapter", run: async () => true };
 const MockAggregatorAdapter = { name: "MockAggregatorAdapter", run: async () => true };
 
